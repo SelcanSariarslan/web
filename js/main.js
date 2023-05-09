6bb37bdd0bb5ab0562be180d8762e6ba94103b9d
SelcanSariarslan <97344497+SelcanSariarslan@users.noreply.github.com>
2023-05-09 13:11:18 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,104 @@
+$(document).ready(function () {
+  
+window.onload = function() {
+  var token = localStorage.getItem("token")
+  
+  console.log(token)
+
+  if(token !== "") {
+    // logged
+    $("#sign").css("display", "none");
+    $("#addRecipe").css("display", "block");
+    $("#logout").css("display", "block");
+    
+  } else {
+    // not logged
+    $("#sign").css("display", "block");
+    $("#addRecipe").css("display", "none");
+    $("#logout").css("display", "none");
+
+  }
+}
+
+$("#login-button").click(function() {
+  var username = $("#username").val()
+  var password = $("#password").val()
+  
+
+  $.getJSON("json/users.json", function(data) {
+    $.each(data.users, function(i, user){
+      if(user.username === username && user.password === password)  {
+          localStorage.setItem("token", user.token)
+          window.location.reload()
+      }
+      // else{
+      //   alert("user not valid!")
+      // }
+  })
+  })
+})
+
+$("#logout").click(function() {
+  localStorage.setItem("token", "")
+  window.location.reload();
+})
+
+
+
+
+  $(".nav-ul li").hover(function() {
+    var isHovered = $(this).is(":hover");
+    if (isHovered) {
+      $(this).children("ul").stop().slideDown(300);
+    } else {
+      $(this).children("ul").stop().slideUp(300);
+    }
+  });
+
+  var food=""
+
+  $("#nav-food").click(function(){
+
+   food = ($("#nav-food").text());
+
+    localStorage.setItem("food", food)
+ window.location.replace("list.html")
+
+    $("#listTitle").text(food)
+    
+  })
+
+  $("#nav-desert").click(function(){
+
+   food = ($("#nav-desert").text());
+   
+
+    localStorage.setItem("food", food)
+ window.location.replace("list.html")
+
+    $("#listTitle").text(food)
+    
+  })
+
+
+
+
+  //like animation
+
+  
+  function like(id) {
+
+    x = $('#'+id).text()
+    num= parseInt(x)
+    sum= num+1
+    $('#'+id).text(sum)
+  }
+
+  $(".heart").on('click touchstart', function(){
+    $(this).toggleClass('is_animating');
+  });
+
+  $(".heart").on('animationend', function(){
+    $(this).toggleClass('is_animating');
+  });
+})
---
+++ js/recipe.js
@@ -0,0 +1,40 @@
+let title = localStorage.getItem("title")
+let number = localStorage.getItem("number")
+food = localStorage.getItem("food")
+
+
+var url =
+"https://onurgozcu.com/food-court/api/food/getFoodDetails?foodId="+number;
+$.getJSON(url, function(data){
+    $.each(data.food, function(i, item){
+        if(item.name === title) {
+            $("#breadcrumb-item").text(item.name)
+            $("#recipeTime").text(item.preparationTime)
+             $("#recipeDifficulty").text(item.difficulty)
+            $("#recipePortion").text(item.portionCount)
+            $("#recipeListName").text(item.name)
+            $("#titleRecipe").text(item.name)
+            $("#writer").text(item.userName)
+            $("#recipeImg").append(
+                '<img src="' + item.image + '"alt="recipe">' )
+
+                $("#writerImg").append(
+                    '<img src="' + item.image + '"alt="profile">' )
+           
+                $.each(item.steps, function(i, step){
+                    $("#recipeList").append(
+                '  <div class="recipeItem"><h5>Step ' + (i+1) +'</h5><span>' +
+                step.content +' </span></div>' )
+            
+            })
+
+            $.each(item.materials, function(i, material){
+                $("#materialContent").append(
+            '<p>'+material.content +'</p>' )
+        
+        })
+                
+
+        }
+    })
+})
